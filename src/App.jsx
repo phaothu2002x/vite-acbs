a60c6124047db404711faf810b1879428d64753f
phaothu2002x <truongthuan21342@gmail.com>
2024-04-12 02:32:24 +0700

    add antd library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Button } from "antd";
+
 function App() {
     return (
         <>
             <div className="text-6xl text-center font-bold text-orange-500">
                 Welcome to Hell Paradise
+            </div>
+            <div className="h-screen flex items-center justify-center">
+                <Button type="primary" className="bg-orange-500 h-[100px] ">
+                    Click To Die ^_^
+                </Button>
             </div>
         </>
     );
